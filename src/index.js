6d7d3271631b8eefc9fefca63463273719421db6
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-05-28 14:29:30 -0400

    adding updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const newTask = document.getElementById("create-task-form")
+const listOfTasks = document.getElementById("tasks")
+newTask.addEventListener("submit", (e) => {
+e.preventDefault()
+
+const newInput = e.target.querySelector("#new-task-description").value
+const newList = document.createElement("li")
+newList.innerText = newInput
+
+listOfTasks.append(newList)
+})
+
 });
+
